51661f4be826f98d2a2886c79705e8d29b005987
Andre Nascimento <1914andre@gmail.com>
2024-09-21 21:15:54 -0300

    feat: adding code base about logic render api backend

--- vite-project/src/components/api_library/requests_Api.jsx
+++ vite-project/src/components/api_library/requests_Api.jsx
@@ -1,6 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import conectorApi from "./conection_Api";
 
 const ApiLibrary = () => { 
-    const [render,setRender]= useEffect([])
+    const [render,setRender]= useState([])
+
+    useEffect(() => {
+            conectorApi.get('/RoteBoocksG').then(({data})=>{
+                setRender(data)
+            })
+            console.log(render)
+    },[''])
+    
+    return(
+        <div className="books-code">
+            {render}
+        </div>
+    )
 }
+export default ApiLibrary
